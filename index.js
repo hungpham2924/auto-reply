f7f7c2ba569aec3f17ff72caab6f5de55575757b
hungpham2924 <anitito123h55@gmail.com>
2024-04-27 17:23:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,5 +17,11 @@ client.on("message", message => {
     return message.channel.send("kys \n https://cdn.discordapp.com/attachments/1207560765490597888/1233706632089702461/kys-keep-yourself-safe.png?ex=662e1256&is=662cc0d6&hm=3928cdb120d18cf9e67d4e2d7afee4de3895aa9b82aed9459f15101838c3abbd&")
   }
   })
+  client.on("message", message => {
+    if(message.content === "wait.") {
+      return message.channel.send("waiting for someone here to get Himeko at my banner and raise her's eidolons to e4")
+    }
+    })
+
 
 client.login(process.env.TOKEN);
