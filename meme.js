39d43b98538680c70f41523108ab181ab121c2cc
hungpham2924 <anitito123h55@gmail.com>
2024-04-27 21:03:38 +0700

    a

--- meme.js
+++ meme.js
@@ -17,12 +17,9 @@ client.on("message", message => {
     return message.channel.send("kys \n https://cdn.discordapp.com/attachments/1207560765490597888/1233706632089702461/kys-keep-yourself-safe.png?ex=662e1256&is=662cc0d6&hm=3928cdb120d18cf9e67d4e2d7afee4de3895aa9b82aed9459f15101838c3abbd&")
   }
   })
-  import arr from 'meme.js';
-  console.log(arr);
-  const random1 = arr[(Math.floor(Math.random()*(arr.length)))];
   client.on("message", message => {
     if(message.content === "g.i") {
-      return message.channel.send(random1)
+      return message.channel.send("waiting for someone here to get Himeko at my banner and raise her's eidolons to e4")
     }
     })
 
