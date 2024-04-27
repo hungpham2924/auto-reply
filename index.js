f364886a19e86e70f18e78b2a3c2fed8e2c289e2
hungpham2924 <anitito123h55@gmail.com>
2024-04-27 22:45:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const app = express();
-const arr = [
-  "waiting for someone here to get Himeko at my banner and raise her's eidolons to e4",
-  "I am the bone of my bat. \n Stellaron is my body , and Trailblaze is my blood.\n I have pulled over a thousand paths. \n Unaware of lost , nor aware of gain. \n Withstood pain to create many paths awaiting Aventurine's arrival. \n I have no regrets. \n This is the only path. \n My whole life. \n Was Unlimited Jade Works."
-]
 app.listen(3000, () => {
   console.log("Project is running");
 })
@@ -24,6 +20,10 @@ client.on("message", message => {
  
   client.on("message", message => {
     if(message.content === "g.i") {
+      const arr = [
+        "waiting for someone here to get Himeko at my banner and raise her's eidolons to e4",
+        "I am the bone of my bat. \n Stellaron is my body , and Trailblaze is my blood.\n I have pulled over a thousand paths. \n Unaware of lost , nor aware of gain. \n Withstood pain to create many paths awaiting Aventurine's arrival. \n I have no regrets. \n This is the only path. \n My whole life. \n Was Unlimited Jade Works."
+      ]
       const random2 = arr[Math.floor(Math.random() * arr.length)];
       return message.channel.send(random2)
     }
