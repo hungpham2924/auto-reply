3d318111de679d46f92d631c4c3ec12ca56b69a3
hungpham2924 <anitito123h55@gmail.com>
2024-04-27 16:47:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 client.on("message", message => {
   if(message.content === "kys.") {
     return message.channel.send("kys") 
-    return message.channel.send('https://cdn.discordapp.com/attachments/1207560765490597888/1233706632089702461/kys-keep-yourself-safe.png?ex=662e1256&is=662cc0d6&hm=3928cdb120d18cf9e67d4e2d7afee4de3895aa9b82aed9459f15101838c3abbd&')
+    return message.channel.send("https://cdn.discordapp.com/attachments/1207560765490597888/1233706632089702461/kys-keep-yourself-safe.png?ex=662e1256&is=662cc0d6&hm=3928cdb120d18cf9e67d4e2d7afee4de3895aa9b82aed9459f15101838c3abbd&")
   }
 })
 
