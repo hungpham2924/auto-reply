be9cd9e5debe63c98cb77b4ebce33b903294ff39
hungpham2924 <anitito123h55@gmail.com>
2024-04-27 21:53:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const arr = require("arr");
 const app = express();
 
 app.listen(3000, () => {
@@ -19,7 +18,7 @@ client.on("message", message => {
   }
   })
 
-  const arr = [
+  let arr = [
     "waiting for someone here to get Himeko at my banner and raise her's eidolons to e4",
     "I am the bone of my bat. \n Stellaron is my body , and Trailblaze is my blood.\n I have pulled over a thousand paths. \n Unaware of lost , nor aware of gain. \n Withstood pain to create many paths awaiting Aventurine's arrival. \n I have no regrets. \n This is the only path. \n My whole life. \n Was Unlimited Jade Works."
   ]
