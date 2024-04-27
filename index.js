a46eb7a1b93fd500dc4a573376db468c6c414b20
hungpham2924 <anitito123h55@gmail.com>
2024-04-27 21:39:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ client.on("message", message => {
   }
   })
 
-  import {arr} from "#meme";
-  console.log(arr);
+  const arr = [
+    "waiting for someone here to get Himeko at my banner and raise her's eidolons to e4",
+    "I am the bone of my bat. \n Stellaron is my body , and Trailblaze is my blood.\n I have pulled over a thousand paths. \n Unaware of lost , nor aware of gain. \n Withstood pain to create many paths awaiting Aventurine's arrival. \n I have no regrets. \n This is the only path. \n My whole life. \n Was Unlimited Jade Works."
+  ]
   const random1 = arr[(Math.floor(Math.random()*(arr.length)))];
   
   client.on("message", message => {
