dad71a3279d8a3d3184acb0765dc3e0459a0f17c
hungpham2924 <anitito123h55@gmail.com>
2024-05-27 19:30:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const arr = [
   "I am the bone of my bat. \n Stellaron is my body , and Trailblaze is my blood.\n I have pulled over a thousand paths. \n Unaware of lost , nor aware of gain. \n Withstood pain to create many paths awaiting Aventurine's arrival. \n I have no regrets. \n This is the only path. \n My whole life. \n Was Unlimited Jade Works.",
   "https://cdn.discordapp.com/attachments/1207560765490597888/1233811212601659473/FB_IMG_1714191168250.png?ex=662f1c7c&is=662dcafc&hm=97c57195a8775c003b5821948b3a49424f2fc290955edb55313f8eecf46532a3&",
   "https://cdn.discordapp.com/attachments/1207560765490597888/1238695662472794122/Khong_Co_Tieu_e32_20240511123233.jpg?ex=664038bb&is=663ee73b&hm=0273540fe948951ed3c27172ff9fbf3874634fafb7f7adee659c2242cf456b6c&",
-  "https://cdn.discordapp.com/attachments/1207560765490597888/1238669783059140618/Khong_Co_Tieu_e38_20240511012345.jpg?ex=664020a1&is=663ecf21&hm=0eba07e46973d168d4e52c22b56e3d1b61aa4d3bfeb7f0406ffc3319a50b5f9b&"
+  "https://cdn.discordapp.com/attachments/1207560765490597888/1238669783059140618/Khong_Co_Tieu_e38_20240511012345.jpg?ex=664020a1&is=663ecf21&hm=0eba07e46973d168d4e52c22b56e3d1b61aa4d3bfeb7f0406ffc3319a50b5f9b&",
+  "https://cdn.discordapp.com/attachments/1207560765490597888/1244628700943876126/Screenshot_20240524_214340_Discord.jpg?ex=6655ce4e&is=66547cce&hm=ee2e2429803603fa69f3f6ee649bf5a9265f4c83bbdc6284e4cc78b68c361fc5&"
 ]
 const express = require("express");
 const app = express();
